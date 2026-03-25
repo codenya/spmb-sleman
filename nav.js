46aa6cff067fe79cb928cf3b83783304ff94fcf7
codenya <3.sahabat.004748@gmail.com>
2026-03-25 14:03:43 +0700

    Refactor nav.js to update navigation links

--- nav.js
+++ nav.js
@@ -9,7 +9,11 @@
         <li><a target="_blank" href="https://vswi.github.io/kalkulatorsleman/PETUNJUK%20TEKNIS%20PENERIMAAN%20MURID%20BARU%20PADA%20JENJANG%20SMP%20DI%20KAB.SLEMAN%20TAHUN%20AJARAN%202025-2026.pdf">Juknis SMPN Sleman</a></li>
         <li><a target="_blank" href="https://vswi.github.io/kalkulatorkotayogya/2025kw3405210.pdf">Juknis 2025 SMPN Kota Yogyakarta</a></li>
         <li><a target="_blank" href="https://vswi.github.io/kalkulatorbantul/Juknis-Bantul-SMPN-keputusan-bupati-2025-226-Juknis-SPMB.pdf">Juknis SMPN Bantul</a></li>
-        <li><a target="_blank" href="https://arsip.spmb.id/2024/sleman/#/020001/statistik">Nilai Terendah 2024 Jalur Prestasi Sleman</a></li>
+        <li><a target="_blank" href="https://sleman.spmb.id/020201/hasil">Nilai Jalur Domisili Wilayah Sleman 2025</a></li> 
+		
+		<li><a target="_blank" href="https://sleman.spmb.id/020001/hasil">Jalur Prestasi Sleman Sleman 2025</a></li>
+		
+		<li><a target="_blank" href="https://arsip.spmb.id/2024/sleman/#/020001/statistik">Nilai Terendah 2024 Jalur Prestasi Sleman</a></li>
         <li><a target="_blank" href="https://arsip.spmb.id/2024/sleman/#/020201/statistik">Nilai Terendah 2024 Jalur Domisili Wilayah Sleman</a></li>
         <li><a target="_blank" href="https://arsip.spmb.id/2024/yogya/#/020701/statistik">Nilai Terendah 2024 Jalur Prestasi Luar Daerah Kota Yogyakarta</a></li>
         <li><a target="_blank" href="https://arsip.spmb.id/2024/yogya/#/020301/statistik">Nilai Terendah 2024 Jalur Zonasi Daerah Kota Yogyakarta</a></li>
